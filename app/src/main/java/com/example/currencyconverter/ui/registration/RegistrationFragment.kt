5f0eb0fb37022af657687db7c880ae901ab7a48b
Dimadyuk <dimadyuk88@gmail.com>
2023-05-23 11:11:50 +0200

    setup navigation

---
+++ app/src/main/java/com/example/currencyconverter/ui/registration/RegistrationFragment.kt
@@ -0,0 +1,52 @@
+package com.example.currencyconverter.ui.registration
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.currencyconverter.R
+import com.example.currencyconverter.databinding.FragmentRegistrationBinding
+
+
+class RegistrationFragment : Fragment() {
+
+    private var _binding: FragmentRegistrationBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvHello.setOnClickListener {
+            findNavController().navigate(R.id.navigation_home)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        fun newInstance() =
+            RegistrationFragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+}
